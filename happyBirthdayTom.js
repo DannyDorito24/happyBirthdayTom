297622e6eb821c10582feb07f4f79e974a399886
DannyDorito24 <75233010+DannyDorito24@users.noreply.github.com>
2021-04-12 17:04:08 -0500

    Un Yandere'd the code

--- happyBirthdayTom.js
+++ happyBirthdayTom.js
@@ -16,43 +16,42 @@ bot.once('ready', () => {
           type: "WATCHING",
       }
   })});
-bot.on('message', message => {
-    console.log(message.author.username, "sent:", message.content);
-    if (message.author.bot) return;
-    if(message.content === 'e') {
-        message.channel.send('e');
-    }
-    if(message.content.includes('!!ban')) {
-        message.channel.send('Ok, user banned')
-    }
-    if(message.content === 'uwu') {
-        message.channel.send('0w0')
-    }
-    if(message.content === 'owo') {
-        message.channel.send('UwU')
-    }
-    if(message.content === 'h') {
-        message.channel.send('h')
-    }
-    if(message.content === 'beef') {
-        message.channel.send('beef')
-    }
-    if(message.content === "!logg") {
-        message.channel.bulkDelete(1);
-        const logU = bot.emojis.cache.find(emoji => emoji.name === "logU");
-        message.channel.send(`${logU}`);
-     }
-    if(message.content ==='!w') {
-  var countDownDate = new Date("May 1, 2021 13:00:00").getTime();
-  var now = new Date().getTime();
-  var distance = countDownDate - now;
-  var weeks = Math.floor(distance / (1000 * 60 * 60 * 24 * 7));
-  var days = Math.floor((distance % (1000 * 60 * 60 * 24 * 7)) / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        
-    message.channel.send("The Marriage between hewwo_owo1 and Tomcuteko is in " + weeks + " Weeks, " + days + " Days, " + hours + " Hours, and "+ minutes + " Minutes.")
+bot.on('msg', message => {
+    console.log(msg.author.username, "sent:", msg.content);
+    if (msg.author.bot) return;
+    switch (msg.content) {
+        case "e":
+            msg.channel.send('e');
+            break;
+        case "uwu":
+            msg.channel.send("0w0");
+            break;
+        case "owo":
+            msg.channel.send("UwU");
+            break;
+        case "h":
+            msg.channel.send("h");
+            break;
+        case "beef":
+            msg.channel.send("beef");
+            break;
+        case "!log":
+            essage.channel.bulkDelete(1);
+            const logU = bot.emojis.cache.find(emoji => emoji.name === "logU");
+            message.channel.send(`${logU}`);
+            break;
+        case "!w":
+            var countDownDate = new Date("May 1, 2021 13:00:00").getTime();
+            var now = new Date().getTime();
+            var distance = countDownDate - now;
+            var weeks = Math.floor(distance / (1000 * 60 * 60 * 24 * 7));
+            var days = Math.floor((distance % (1000 * 60 * 60 * 24 * 7)) / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            message.channel.send("The Marriage between hewwo_owo1 and Tomcuteko is in " + weeks + " Weeks, " + days + " Days, " + hours + " Hours, and "+ minutes + " Minutes.")
+        default:
+            return;
     }
 });
 let scheduledMessage = new cron.CronJob('0 13 * * *', () => {
